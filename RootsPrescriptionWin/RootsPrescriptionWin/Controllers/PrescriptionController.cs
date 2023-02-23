27370b0bc60e78bec516433c99702e1d8f515087
EMEAAD\vbakke <vegard.bakke@soprasteria.com>
2023-02-23 21:41:40 +0100

    Initial comitt (incluing default WeatherForecast)

---
+++ RootsPrescriptionWin/RootsPrescriptionWin/Controllers/PrescriptionController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace RootsPrescriptionWin.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PrescriptionController : ControllerBase
+    {
+        private readonly ILogger<PrescriptionController> _logger;
+
+        public PrescriptionController(ILogger<PrescriptionController> logger)
+        {
+            _logger = logger;
+            //_logger.BeginScope("PrescriptionController");
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<string> Ping()
+        {
+            const string message = "--CHANGE ME--";
+
+            //_logger.Information(helloWorld);
+            return message;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<string> Fetch(string id)
+        {
+            if (id == null || id == "404")
+            {
+                return NotFound();
+            } else
+            {
+                _logger.LogInformation("Request: " + id);
+                return "Request: " + id;
+            }
+
+        }
+
+    }
+}
