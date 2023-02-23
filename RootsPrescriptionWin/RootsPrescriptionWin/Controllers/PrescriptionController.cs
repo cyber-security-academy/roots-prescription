17ec3b6955842ac12145f27dcb83b3ca5c28bf65
EMEAAD\vbakke <vegard.bakke@soprasteria.com>
2023-02-23 22:41:54 +0100

    Added dummy FileStorageService

--- RootsPrescriptionWin/RootsPrescriptionWin/Controllers/PrescriptionController.cs
+++ RootsPrescriptionWin/RootsPrescriptionWin/Controllers/PrescriptionController.cs
@@ -1,48 +1,49 @@
-﻿using System.Net;
-using System.Web;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging.Abstractions;
+﻿using Microsoft.AspNetCore.Mvc;
+using RootsPrescriptionWin.FileStorage;
 
-namespace RootsPrescriptionWin.Controllers
+namespace RootsPrescriptionWin.Controllers;
+
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class PrescriptionController : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class PrescriptionController : ControllerBase
+    private readonly ILogger<PrescriptionController> _logger;
+    private readonly IFileStorageService _filestorage;
+
+    public PrescriptionController(ILogger<PrescriptionController> logger, IFileStorageService filestorage)
     {
-        private readonly ILogger<PrescriptionController> _logger;
+        _logger = logger;
+        _filestorage = filestorage;
+    }
 
-        public PrescriptionController(ILogger<PrescriptionController> logger)
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<string> Ping()
+    {
+        const string message = "--CHANGE ME--";
+
+        //_logger.Information(helloWorld);
+        return message;
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<string> Fetch(string id)
+    {
+        if (id == null || id == "404")
         {
-            _logger = logger;
-            //_logger.BeginScope("PrescriptionController");
+            return NotFound();
         }
-
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<string> Ping()
+        else
         {
-            const string message = "--CHANGE ME--";
-
-            //_logger.Information(helloWorld);
-            return message;
-        }
-
-
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<string> Fetch(string id)
-        {
-            if (id == null || id == "404")
-            {
-                return NotFound();
-            } else
-            {
-                _logger.LogInformation("Request: " + id);
-                return "Request: " + id;
-            }
-
+            _logger.LogInformation("Request: " + id);
+            string file = _filestorage.GetFile(id);
+            return file;
         }
 
     }
+
 }
