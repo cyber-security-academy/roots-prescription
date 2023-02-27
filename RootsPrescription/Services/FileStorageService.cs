ebdd1aa4614d3a481590a401d6c9bfa88dd5af51
Marit Iren <maritiren@users.noreply.github.com>
2023-02-27 08:58:07 +0100

    Add initial scaffolded API (#1)
    * Initial commit (including default WeatherForecast)
    
    * Add dummy FileStorageService
    
    * Addnear empty Docker project in 'RootsPrescription'.
    (Contians VS default WeatherForecast app)
    
    * Move Windows app logic into Docker app
    
    * Add Health check endpoint
    
    ---------
    
    Co-authored-by: EMEAAD\vbakke <vegard.bakke@soprasteria.com>

---
+++ RootsPrescription/Services/FileStorageService.cs
@@ -0,0 +1,16 @@
+﻿namespace RootsPrescriptionWin.FileStorage;
+public class FileStorageService : IFileStorageService
+{
+    private readonly ILogger<FileStorageService> _logger;
+
+    public FileStorageService(ILogger<FileStorageService> logger)
+    {
+        _logger = logger;
+    }
+
+    public string GetFile(string id)
+    {
+        _logger.LogInformation($"Loading file: {id}");
+        return "File: " + id;
+    }
+}
