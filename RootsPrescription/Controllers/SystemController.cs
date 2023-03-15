a04de277a5487e309169694b3a451c796c1a9f15
EMEAAD\vbakke <vegard.bakke@soprasteria.com>
2023-03-15 21:58:56 +0100

    Added /api/System/Ping

---
+++ RootsPrescription/Controllers/SystemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using RootsPrescription.Database;
+using RootsPrescription.FileStorage;
+using RootsPrescription.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using SimpleHashing.Net;
+
+namespace RootsPrescription.Controllers;
+
+
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class SystemController : ControllerBase
+{
+    public SystemController()
+    {
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<string> Ping()
+    {
+        const string message = "--CHANGE ME--";
+
+        return message;
+    }
+
+
+}
