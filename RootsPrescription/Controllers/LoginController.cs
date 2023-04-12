b37c6fe0fff98af7be546c170517421f1a5a416c
Marit Iren <maritiren@users.noreply.github.com>
2023-04-12 16:55:49 +0200

    Update RootsPrescription/Controllers/LoginController.cs

--- RootsPrescription/Controllers/LoginController.cs
+++ RootsPrescription/Controllers/LoginController.cs
@@ -51,7 +51,7 @@ public class LoginController : ControllerBase
         // Cookie authentication (.Net cookie)
         await SetAuthenticationCookie(claims, expiryMinutes);
 
-        // JWT bearar token authentication
+        // JWT bearer token authentication
         string token = GenerateToken(claims, expiryMinutes);
 
         _logger.LogInformation($"User logged in: {user.ToString()}");
